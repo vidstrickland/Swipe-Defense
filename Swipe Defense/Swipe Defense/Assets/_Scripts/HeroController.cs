ee8422d4b8f70ff260ce6334c3df0ee1402214e6
vid <vidkid7@gmail.com>
2017-06-20 23:17:29 -0400

    Initialized Repository / Added Swipe Recognition and Hero Controller
    Initialized Repository / Added Swipe Recognition and Hero Controller

---
+++ Swipe Defense/Swipe Defense/Assets/_Scripts/HeroController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+
+	//TODO
+	//Rotate Hero to match swipe. (Sprite change? Actual rotation? Both?)
+	//Add collision rules for two projectile types (Fireball, Arrow)
+
+	void Start ()
+	{
+		SwipeManager.OnSwipeDetected += OnSwipeDetected;
+	}
+
+	void OnSwipeDetected (Swipe direction, Vector2 swipeVelocity)
+	{
+		print ("Direction: " + direction + " Velocity: " + swipeVelocity);
+	}
+
+}
